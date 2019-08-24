750c4632965753e5d2546cf7d2702bc839053f28
Luiza <luiza.niemiec@gmail.com>
2019-08-24 18:31:25 +0200

    color changing

--- js/general.js
+++ js/general.js
@@ -2,16 +2,12 @@ const btnItem = document.querySelectorAll('.btn-item');
 const btnItemCon = document.querySelectorAll('.btn-project-item');
 
 function selectItem(e) {
-    removeBorder();
     removeShow();
     this.classList.add('btn-border');
     const btnContent = document.querySelector(`#${this.id}-project`);
     btnContent.classList.add('show');
 }
 
-function removeBorder() {
-    btnItem.forEach(item => item.classList.remove('btn-border'));
-}
 
 function removeShow() {
     btnItemCon.forEach(item => item.classList.remove('show'));
